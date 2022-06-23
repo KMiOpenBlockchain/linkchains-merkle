ab2bf0387f89237d5b831e45033c3407c41d882e
Allan Third <athird@users.noreply.github.com>
2022-06-23 11:35:38 +0100

    Workaround for granular metadata roundtrip to other RDF formats.

---
+++ frames/quadmetadata-frame.js
@@ -0,0 +1,17 @@
+quadMetadataFrame = {
+    "@context": {
+        "@vocab": "https://blockchain.open.ac.uk/vocab/"
+    },
+    "graphs": [{
+        "graphname": {},
+        "proofs": {}
+    }],
+    "metadata": {
+        "@id": {},
+        "anchor": {
+            "settings": {}
+        },
+        "index": {},
+        "indexhash": {}
+    }
+}
